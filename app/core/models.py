fecd29d016eed1af45d7b3d1c496e1f051ee5b28
clioo <jesus_acosta1996@hotmail.com>
2020-04-29 16:29:03 -0600

    expenses and budgets models ready to be tested

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+import datetime
+
+
+def validate_cero(value):
+    if value <= 0:
+        raise ValidationError(
+            _(f'{value} not valid, it must be greater than 0.')
+        )
+
+
+def year_choices():
+    return [(r, r) for r in range(current_year(), current_year() + 10)]
+
+
+def current_year():
+    return datetime.date.today().year
 
 
 class UserManager(BaseUserManager):
@@ -30,3 +48,68 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
     USERNAME_FIELD = 'email'
+
+
+class BaseBudget(models.Model):
+    """Base budget model that validates amount"""
+    description = models.CharField(max_length=255,
+                                   verbose_name='Descripción')
+    amount = models.FloatField(verbose_name='Cantidad',
+                               validators=[validate_cero])
+    user = models.ForeignKey('User', on_delete=models.PROTECT,
+                             verbose_name='Usuario')
+
+
+class BaseTag(models.Model):
+    user = models.ForeignKey('User', verbose_name=_('User'))
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+
+
+class AnnualBudget(BaseBudget):
+    year = models.IntegerField(verbose_name=_('Year'), choices=year_choices)
+
+    class Meta:
+        unique_together = ['year', 'user']
+
+
+class ExpensesTag(BaseTag):
+
+    def __str__(self):
+        return _('Expense') + ' ' + self.name
+
+
+class MonthBudget(BaseBudget):
+    annual_budget = models.ForeignKey('AnnualBudget', on_delete=models.PROTECT,
+                                      verbose_name=_('Annual budget'))
+    expenses_tag = models.ForeignKey('ExpensesTag', on_delete=models.PROTECT,
+                                     verbose_name=_('Expenses tag'))
+
+    def __str__(self):
+        return _('Budget for') + ' ' + self.expenses_tag.name
+
+class Expense(BaseBudget):
+    """Single expense, detailed"""
+    date = models.DateField(verbose_name=_('Date'))
+    expenses_tag = models.ForeignKey(
+        'ExpensesTag',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        verbose_name=_('Expenses tag')
+    )
+    month_budget = models.ForeignKey(
+        'MonthBudget',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        verbose_name=_('Month budget')
+    )
+
+    def save(self, *args, **kwargs):
+        if self.expenses_tag and self.month_budget:
+            raise ValueError(_("""You can choose just one option: 
+                                  month budget or expenses tag"""))
+        elif not self.expenses_tag and not self.month_budget:
+            raise ValueError(_("""You need to choose just option: 
+                                  month budget or expenses tag"""))
+        super(Expense, self).save(*args, **kwargs)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 
 def sample_user(email='testuser@tes.com', password='password123'):
@@ -39,3 +40,111 @@ class UserModelTests(TestCase):
         """Test that the str generated after creating a user is correct"""
         user = sample_user()
         self.assertEqual(str(user), user.email)
+
+
+class ModelTests(TestCase):
+    """Tests for models"""
+
+    def setUp(self):
+        self.user = sample_user()
+        self.anual_budget = models.AnnualBudget(
+            amount=2000000,
+            year='2025',
+            user=self.user
+        )
+        self.anual_budget.save()
+        self.expenses_tag1 = ExpensesTag(name='cheve', user=self.user)
+        self.expenses_tag1.save()
+        self.month_budget = MonthBudget(
+            description='Cerveza para los fines de semana',
+            amount=1000,
+            anual_budget=anual_budget,
+            expenses_tag=expenses_tag1,
+            user=self.user
+        )
+
+    def test_create_anual_budget_success(self):
+        """Test create successfully anual budget"""
+        anual_budget = models.AnnualBudget(
+            amount=200000,
+            year='2020'
+        )
+        anual_budget.save()
+        self.assertEqual(str(anual_budget), 
+                         f'Presupuesto del {anual_budget.year}')
+
+    def test_create_repeated_anual_budget_error(self):
+        """Test that users can't create repeated anual budgets"""
+        anual_budget1 = models.AnnualBudget(
+            amount=200000,
+            year='2020',
+            user=self.user
+        )
+        anual_budget2 = models.AnnualBudget(
+            amount=300000,
+            year='2020',
+            user=self.user
+        )
+        # To do: assert that error is raised
+
+    def test_create_expenses_tag_successful(self):
+        expenses_tag1 = ExpensesTag(name='cheve', user=self.user)
+        expenses_tag2 = ExpensesTag(name='tdc', user=self.user)
+        expenses_tag1.save()
+        expenses_tag2.save()
+        self.assertEqual(str(expenses_tag1), expenses_tag1.name)
+        self.assertEqual(str(expenses_tag2), expenses_tag2.name)
+
+    def test_create_month_budget_success(self):
+        """Test create a monthly budget with certain tag"""
+        anual_budget = models.AnnualBudget(
+            amount=2000000,
+            year='2020',
+            user=self.user
+        )
+        anual_budget.save()
+        expenses_tag1 = ExpensesTag(name='cheve', user=self.user)
+        expenses_tag1.save()
+        month_budget = MonthBudget(
+            description='Cerveza para los fines de semana',
+            amount=1000,
+            anual_budget=anual_budget,
+            expenses_tag=expenses_tag1,
+            user=self.user
+        )
+        month_budget.save()
+        self.assertEqual(str(month_budget),
+                         f'Presupuesto para {month_budget.expenses_tag.name}')
+
+    def test_create_month_budget_amount_0(self):
+        """Test that you can't create a month budget with 0 amount"""
+        #To do: assert when fails
+        pass
+
+    def test_create_expense_success(self):
+        expense1 = Expense(
+            amount=300,
+            date='2025-12-12',
+            month_budget=self.month_budget,
+            user=self.user
+        )
+        expense1.save()
+        expense2 = Expense(
+            amount=200,
+            date='2025-12-12',
+            expenses_tag=self.expenses_tag1,
+            user=self.user
+        )
+        expense2.save()
+        self.assertEqual(str(expense1),
+                         expense1.month_budget.expenses_tag.name)
+        self.assertEqual(str(expense2),
+                         expense2.expenses_tag.name)
+
+    def test_no_tag_or_month_budget_error(self):
+        with self.assertRaises(ValueError):
+            expense1 = Expense(
+                amount=200,
+                date='2025-12-12',
+                user=self.user
+            )
